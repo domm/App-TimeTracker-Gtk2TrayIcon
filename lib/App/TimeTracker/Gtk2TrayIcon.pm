f1be14548dd6054f64ab6953fafd9ebb49fb0214
Thomas Klausner <domm@plix.at>
2011-08-21 11:57:16 +0200

    perltidy

--- lib/App/TimeTracker/Gtk2TrayIcon.pm
+++ lib/App/TimeTracker/Gtk2TrayIcon.pm
@@ -17,46 +17,57 @@ sub run {
 
     my $storage_location = App::TimeTracker::Proto->new->home;
 
-    my $lazy = -e 'share/lazy.png' ? 'share/lazy.png' : dist_file('App-TimeTracker-Gtk2TrayIcon','lazy.png');
-    my $busy = -e 'share/busy.png' ? 'share/busy.png' : dist_file('App-TimeTracker-Gtk2TrayIcon','busy.png');
-    my $img= Gtk2::Image->new_from_file($lazy);
-    my $window= Gtk2::TrayIcon->new(__PACKAGE__);
+    my $lazy =
+        -e 'share/lazy.png'
+        ? 'share/lazy.png'
+        : dist_file( 'App-TimeTracker-Gtk2TrayIcon', 'lazy.png' );
+    my $busy =
+        -e 'share/busy.png'
+        ? 'share/busy.png'
+        : dist_file( 'App-TimeTracker-Gtk2TrayIcon', 'busy.png' );
+    my $img      = Gtk2::Image->new_from_file($lazy);
+    my $window   = Gtk2::TrayIcon->new(__PACKAGE__);
     my $eventbox = Gtk2::EventBox->new;
     $eventbox->add($img);
 
     my $current;
-    my $t = AnyEvent->timer(after => 0, interval => 5, cb => sub {
-        my $task = App::TimeTracker::Data::Task->current($storage_location);
-        if ($task) {
-            $img->set_from_file($busy);
-            $current = $task->say_project_tags;
-        }
-        else {
-            $img->set_from_file($lazy);
-            $current = 'nothing';
-        }
-    });
+    my $t = AnyEvent->timer(
+        after    => 0,
+        interval => 5,
+        cb       => sub {
+            my $task =
+                App::TimeTracker::Data::Task->current($storage_location);
+            if ($task) {
+                $img->set_from_file($busy);
+                $current = $task->say_project_tags;
+            }
+            else {
+                $img->set_from_file($lazy);
+                $current = 'nothing';
+            }
+        } );
 
-    $eventbox->signal_connect( 'enter-notify-event' => sub {
-        unless ($current eq 'nothing') {
+    $eventbox->signal_connect(
+        'enter-notify-event' => sub {
+            unless ( $current eq 'nothing' ) {
 
-            my $dialog = Gtk2::MessageDialog->new ($window,
-                [qw/modal destroy-with-parent/],
-                'other',
-                'none',
-                $current
-            );
-    
-            $dialog->set_decorated (0);
-            $dialog->set_gravity ('south-west');
+                my $dialog =
+                    Gtk2::MessageDialog->new( $window,
+                    [qw/modal destroy-with-parent/],
+                    'other', 'none', $current );
 
-            my $t = AnyEvent->timer(after => 5, cb => sub {
+                $dialog->set_decorated(0);
+                $dialog->set_gravity('south-west');
+
+                my $t = AnyEvent->timer(
+                    after => 5,
+                    cb    => sub {
+                        $dialog->destroy;
+                    } );
+                my $retval = $dialog->run;
                 $dialog->destroy;
-            });
-            my $retval = $dialog->run;
-            $dialog->destroy;
-        }
-    });
+            }
+        } );
 
     $window->add($eventbox);
     $window->show_all;
